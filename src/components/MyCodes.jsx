eae2daca91c06c9389cad9452d4b289a46b4f59c
ahmed salih <ahmed.elabid.elsalih@gmail.com>
2022-05-20 20:19:57 +0300

    final project

---
+++ src/components/MyCodes.jsx
@@ -0,0 +1,78 @@
+// this object for my projects
+const a = [
+
+  {
+    id: 0,
+    name: "To-do-app",
+    demo: "https://github.com/ahmedelabidelsalih/Todo-App",
+    source: "https://github.com/ahmedelabidelsalih/Todo-App",
+    tools: "React",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/165645073-cac0a728-8c2a-4886-890b-aa73341b22ee.png",
+  },
+  {
+    id: 1,
+    name: "RE:Coded",
+    demo: "https://ahmedelabidelsalih.github.io/1-my-challenge-with-re-coded/",
+    source: "https://github.com/ahmedelabidelsalih/1-my-challenge-with-re-coded",
+    tools: "Html, Css, JS",
+    banar:
+      "https://github.com/ReCoded-Org/FWD-Coding-Challenge/raw/master/imgs/codingChallenge1.jpg",
+  },
+  {
+    id: 2,
+    name: "website",
+    demo: "https://ahmedelabidelsalih.github.io/first/",
+    source: "https://github.com/ahmedelabidelsalih/first",
+    tools: "Html, Css, JS",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162160804-7350e209-ee38-44c2-8aea-addd1f3621b3.png",
+  },
+  {
+    id: 3,
+    name: "Survey Form",
+    demo: "https://ahmedelabidelsalih.github.io/Survey-Form/",
+    source: "https://github.com/ahmedelabidelsalih/Survey-Form",
+    tools: "Html, Css, JS",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162165409-34e36642-24ad-4d9f-b23e-5e15567bde4e.png",
+  },
+  {
+    id: 0,
+    name: "Zarproken compane",
+    demo: "https://ahmedelabidelsalih.github.io/third/",
+    source: "https://github.com/ahmedelabidelsalih/third",
+    tools: "",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162168992-0788980b-42ca-411c-958d-023bdfc10e27.png",
+  },
+  {
+    id: 1,
+    name: "Kasper website",
+    demo: " https://ahmedelabidelsalih.github.io/two2/",
+    source: "https://github.com/ahmedelabidelsalih/two2",
+    tools: "Html, Css, JS",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162170582-f126b426-a54d-4513-ab20-fa082c0b20f7.png",
+  },
+  {
+    id: 2,
+    name: "Dr.Norman",
+    demo: "https://ahmedelabidelsalih.github.io/free-code-camp1/",
+    source: "https://github.com/ahmedelabidelsalih/free-code-camp1",
+    tools: "Html, Css, JS",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162190020-f44629da-fb91-45ef-a5b0-90fa844f50c1.png",
+  },
+  {
+    id: 3,
+    name: "Product-Landing-Page",
+    demo: "https://ahmedelabidelsalih.github.io/Product-Landing-Page/",
+    source: "https://github.com/ahmedelabidelsalih/Product-Landing-Page",
+    tools: "Html, Css, JS",
+    banar:
+      "https://user-images.githubusercontent.com/85963951/162192327-a64d48a8-2584-46fc-90ae-af864e4e18c1.png",
+  },
+];
+
+export default a;
---
+++ src/pages/Skills.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+// Skills component
+function Skills() {
+  return (
+    <div>
+      <div className="theam1 skills">
+        <div className="ch1"></div>
+        <div className="ch2"></div>
+        <div className="ch3"></div>
+        <div className="ch4"></div>
+        <div className="ch5"></div>
+        <div className="ch6"></div>
+        <div className="ch7"></div>
+        <div className="ch8"></div>
+      </div>
+
+      <div className="bowl_3items_contentm ">
+        <div className="bowl_theam3_skills">
+          <div className="theam3">
+            <div className="ch1"></div>
+            <div className="ch2"></div>
+            <div className="ch3"></div>
+            <div className="ch4"></div>
+          </div>
+          <div className="theam3 theam3_row">
+            <div className="ch1"></div>
+            <div className="ch2"></div>
+            <div className="ch3"></div>
+            <div className="ch4"></div>
+          </div>
+        </div>
+
+        <div className="bowl_theam3_skills bowl_theam3_bottom_skills">
+          <div className="theam3">
+            <div className="ch1"></div>
+            <div className="ch2"></div>
+            <div className="ch3"></div>
+            <div className="ch4"></div>
+          </div>
+          <div className="theam3 theam3_row">
+            <div className="ch1"></div>
+            <div className="ch2"></div>
+            <div className="ch3"></div>
+            <div className="ch4"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bowl_text_description_skill">
+        <div className="title_seaction_skills">
+          <span>Skills</span>
+        </div>
+
+        <div className="all_skills_me">
+          <div className="front_end">
+            <span className="title_skil">Basic</span>
+            <div className="lines_between_skil"></div>
+            <span>Html</span>
+            <span>Css</span>
+            <span>Javascript</span>
+            
+            
+            
+          </div>
+          <div className="Framework">
+            <span className="title_skil">Framework</span>
+            <div className="lines_between_skil"></div>
+            <span>Bootstrap</span>
+            <span>React</span>
+            <span>Redux.js</span> 
+            <span>React native</span> 
+            <span> Responsive Web Design</span> 
+         
+          </div>
+          <div className="others">
+            <span className="title_skil">Tools</span>
+            <div className="lines_between_skil"></div>
+            <span>Git</span>
+            <span>GitHub</span>
+            <span>sass</span>
+            <span>oop</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
